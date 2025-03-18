e291d8a754d9405618ad0c50083c9452d8572d92
esther0046 <ghkfu910384@naver.com>
2025-03-18 23:10:06 +0900

    20250318
    - scripts 합치기

--- Assets/Scripts/WhitePlayer/PlayerInteractionZone.cs
+++ Assets/Scripts/WhitePlayer/PlayerInteractionZone.cs
@@ -63,3 +63,28 @@ public class WhitePlayerInteractionZone : MonoBehaviour
     }
 }
 
+public class WhitePlayerReviveInteractable : MonoBehaviour, IInteractable
+{
+    private WhitePlayerController whitePlayer;
+
+    private void Awake()
+    {
+        whitePlayer = GetComponentInParent<WhitePlayerController>();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // 기절 상태라면 부활 로직 실행
+            if (whitePlayer.currentState == WhitePlayerState.Stun)
+            {
+                // 체력을 20으로 회복
+                whitePlayer.currentHealth = 20;
+                // Revive() 메서드 호출
+                whitePlayer.Revive();
+                Debug.Log("플레이어 부활 상호작용");
+            }
+        }
+    }
+}
