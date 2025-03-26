6c57c17528c4d7229d265c28df3a6ae9b4c5dce1
yuno7607 <sj5051715@gmail.com>
2025-03-26 09:48:57 +0900

    몬스터 수정 & 몬스터 clear시 문 생성
    enemyAI.cs
    애니메이션 좌우 등록
    monsterSpawner.cs, SpawnArea.cs
    Data값 가져오도록 수정
    몬스터 전부 처치 시 Data 대로 문 생성
    
    TMUL-76

---
+++ Assets/Scripts/Stage/Door Scriptable/DoorSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDoorSettings", menuName = "Door Settings")]
+public class DoorSettings : ScriptableObject
+{
+    // Resources 폴더에 있어야 PhotonNetwork.Instantiate가 올바르게 작동합니다.
+    public GameObject doorPrefab;
+
+    // 문이 생성될 위치 및 회전값 설정 목록
+    public DoorSpawnSetting[] doorSpawnSettings;
+
+    // 모든 플레이어가 상호작용한 후 대기 시간 (초)
+    public float waitTime = 5f;
+}
