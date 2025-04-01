a556e9ef3b882c7d914726fab5588783d3eb33d0
wasabi2342 <sj5051715@gmail.com>
2025-04-02 04:24:01 +0900

    수정

--- Assets/Scripts/Enemy/Behaviour Tree/MonsterSpawner.cs
+++ Assets/Scripts/Enemy/Behaviour Tree/MonsterSpawner.cs
@@ -1,14 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
 
 public class MonsterSpawner : MonoBehaviourPun
 {
-    [Header("몬스터 프리팹 (Resources 폴더 내에 있어야 함)")]
-    public GameObject[] monsterPrefabs;
-
-    [Header("SpawnArea 참조 (미지정 시 부모에서 자동 할당)")]
     public SpawnArea spawnArea;
 
     private void Awake()
@@ -23,26 +17,26 @@ public class MonsterSpawner : MonoBehaviourPun
         }
     }
 
-    // 외부에서 스폰할 몬스터 수를 전달받아 몬스터를 생성
-    public void SpawnMonsters(int spawnCount)
+    // [변경] MonsterSpawnInfo 배열을 받아서 각 몬스터를 개별적으로 스폰
+    public void SpawnMonsters(MonsterSpawnInfo[] monsterSpawnInfos)
     {
         if (!PhotonNetwork.IsMasterClient)
             return;
 
-        for (int i = 0; i < spawnCount; i++)
+        foreach (var info in monsterSpawnInfos)
         {
-            foreach (var prefab in monsterPrefabs)
+            // info.count만큼 반복해서 스폰
+            for (int i = 0; i < info.count; i++)
             {
-                // SpawnArea 내에서 랜덤한 스폰 위치 결정
                 Vector3 spawnPoint = spawnArea.GetRandomPointInsideArea();
 
-                // 네트워크 객체로 몬스터 생성 (prefab.name은 Resources 폴더 내 프리팹 이름과 일치해야 함)
-                GameObject monster = PhotonNetwork.Instantiate(prefab.name, spawnPoint, Quaternion.identity);
+                // Resources 폴더 내 prefabName으로 네트워크 Instantiate
+                GameObject monster = PhotonNetwork.Instantiate(info.monsterPrefabName, spawnPoint, Quaternion.identity);
 
-                // 생성된 몬스터를 SpawnArea의 자식으로 설정
+                // SpawnArea를 부모로 설정
                 monster.transform.SetParent(spawnArea.transform);
 
-                // 다른 클라이언트에서도 동일한 부모 관계를 갖도록 RPC 호출
+                // 다른 클라이언트에서도 동일하게 부모 설정
                 int monsterViewID = monster.GetComponent<PhotonView>().ViewID;
                 int parentViewID = spawnArea.photonView.ViewID;
                 photonView.RPC("RPC_SetParent", RpcTarget.OthersBuffered, monsterViewID, parentViewID);
--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -6,14 +6,12 @@ public class StageManager : MonoBehaviourPun
 {
     [Header("Prefabs (Resources 폴더에 있어야 함)")]
     public string spawnAreaPrefabName = "SpawnArea"; // SpawnArea 프리팹 이름
-    public string doorPrefabName = "DoorPrefab";
 
     [Header("Stage Settings")]
     public StageSettings currentStageSettings; // 현재 스테이지에 해당하는 설정
     public Quaternion spawnAreaRotation = Quaternion.identity; // 기본 회전 값
 
     private List<GameObject> spawnAreaInstances = new List<GameObject>();
-    private bool isDoorSpawned = false;
 
     private void Start()
     {
@@ -23,56 +21,42 @@ public class StageManager : MonoBehaviourPun
         }
     }
 
-   private void Update()
-    {
-        if (PhotonNetwork.IsMasterClient && AreAllMonstersCleared() && !isDoorSpawned)
-        {
-            SpawnDoor();
-        }
-    }
-
     void SpawnStage()
     {
         foreach (SpawnAreaSetting setting in currentStageSettings.spawnAreaSettings)
         {
-            GameObject spawnAreaInstance = PhotonNetwork.Instantiate(spawnAreaPrefabName, setting.position, spawnAreaRotation);
+            // 1) 스폰 영역 프리팹 생성
+            GameObject spawnAreaInstance = PhotonNetwork.Instantiate(
+                spawnAreaPrefabName,
+                setting.position,
+                spawnAreaRotation
+            );
             spawnAreaInstances.Add(spawnAreaInstance);
-            Debug.Log("Spawned SpawnArea: " + spawnAreaInstance.name + " at position: " + setting.position);
 
-            // SpawnArea 컴포넌트에 반경 설정
+            // 2) 반경 설정
             SpawnArea area = spawnAreaInstance.GetComponent<SpawnArea>();
             if (area != null)
             {
                 area.SetRadius(setting.radius);
-                Debug.Log("Assigned radius: " + setting.radius + " to " + spawnAreaInstance.name);
-            }
-            else
-            {
-                Debug.LogWarning("SpawnArea 컴포넌트를 찾을 수 없습니다 in " + spawnAreaInstance.name);
             }
 
-            // MonsterSpawner 컴포넌트를 자식에서 찾도록 수정
+            // 3) MonsterSpawner 찾아서 몬스터 스폰
             MonsterSpawner spawner = spawnAreaInstance.GetComponentInChildren<MonsterSpawner>();
             if (spawner != null)
             {
-                spawner.SpawnMonsters(setting.monsterSpawnCount);
-                Debug.Log("Spawned monsters using count: " + setting.monsterSpawnCount + " for " + spawnAreaInstance.name);
-            }
-            else
-            {
-                Debug.LogWarning("MonsterSpawner 컴포넌트를 찾을 수 없습니다 in " + spawnAreaInstance.name);
+                spawner.SpawnMonsters(setting.monsterSpawnInfos);
             }
         }
     }
-    void SpawnDoor()
-    {
-        isDoorSpawned = true;
-        // PhotonNetwork.Instantiate(문 프리팹, 위치, 각도);
-        var doorObj = PhotonNetwork.Instantiate(doorPrefabName, new Vector3(0, 0, 0), Quaternion.identity);
-        Debug.Log("Door spawned!");
-    }
+
     public bool AreAllMonstersCleared()
     {
-        return EnemyAI.ActiveMonsterCount <= 0;
+        Debug.Log("ActiveMonsterCount: " + EnemyAI.ActiveMonsterCount);
+        bool cleared = EnemyAI.ActiveMonsterCount == 0;
+        if (cleared)
+        {
+            Debug.Log("모든 몬스터가 제거되었습니다.");
+        }
+        return cleared;
     }
 }
