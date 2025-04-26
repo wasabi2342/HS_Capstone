34323aae0aa2e0f0b08811dad9390470594d04ac
JMchoi0404 <whdalschl815@naver.com>
2025-04-27 01:21:22 +0900

    사운드 크기조정

--- Assets/Scripts/SoundEvent/GhoulSoundEvent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GhoulSoundEvent : MonoBehaviour
-{
-    public void GhoulSoundEffect(string source)
-    {
-        AudioManager.Instance.PlayOneShot($"event:/Character/Monster/{source}", transform.position);
-    }
-}
