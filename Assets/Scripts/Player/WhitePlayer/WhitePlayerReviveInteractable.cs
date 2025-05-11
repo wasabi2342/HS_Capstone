de5f10268b1a85ed255baaddcc0cf7315dedda1f
LeeEunSeok <a4121240@naver.com>
2025-05-11 18:54:14 +0900

    다른팀이면 부활 x

--- Assets/Scripts/Player/WhitePlayer/WhitePlayerReviveInteractable.cs
+++ Assets/Scripts/Player/WhitePlayer/WhitePlayerReviveInteractable.cs
@@ -5,15 +5,27 @@ using UnityEngine.InputSystem;
 public class WhitePlayerReviveInteractable : GaugeInteraction
 {
     private WhitePlayerController whitePlayer;
+    private PhotonView otherPhotonView;
+    private PhotonView localPhotonView;
 
     private void Awake()
     {
         whitePlayer = GetComponentInParent<WhitePlayerController>();
+        localPhotonView = whitePlayer.GetComponent<PhotonView>();
     }
 
     public override void OnInteract(InputAction.CallbackContext ctx)
     {
-        base.OnInteract(ctx);
+        if (whitePlayer.currentState != WhitePlayerState.Stun || otherPhotonView == null)
+        {
+            return;
+        }
+
+        // 같은 팀일 때만 부활 상호작용
+        if (IsSameTeam(localPhotonView, otherPhotonView))
+        {
+            base.OnInteract(ctx);
+        }
         //whitePlayer.HandleReviveInteraction(ctx);
     }
 
@@ -24,6 +36,10 @@ public class WhitePlayerReviveInteractable : GaugeInteraction
             return;
         }
 
+        PhotonView otherView = other.GetComponentInParent<PhotonView>();
+        if (otherView.IsMine)
+            otherPhotonView = otherView;
+
         Debug.Log("부활 상호작용 enter");
 
         base.OnTriggerEnter(other);
@@ -31,6 +47,11 @@ public class WhitePlayerReviveInteractable : GaugeInteraction
 
     protected override void OnTriggerExit(Collider other)
     {
+        if (whitePlayer.currentState != WhitePlayerState.Stun)
+        {
+            return;
+        }
+
         base.OnTriggerExit(other);
 
         Debug.Log("부활 상호작용 exit");
@@ -57,5 +78,26 @@ public class WhitePlayerReviveInteractable : GaugeInteraction
         base.OnStartedEvent();
 
         Debug.Log("부활 상호작용 start");
+    }
+    private bool IsSameTeam(PhotonView localView, PhotonView otherView)
+    {
+        if (!TryGetTeamId(localView, out int myTeamId) || !TryGetTeamId(otherView, out int otherTeamId))
+        {
+            Debug.Log("TeamId가 설정되지 않았습니다.");
+            return false;
+        }
+
+        return myTeamId == otherTeamId;
+    }
+
+    private bool TryGetTeamId(PhotonView view, out int teamId)
+    {
+        if (view.Owner.CustomProperties.TryGetValue("TeamId", out object value))
+        {
+            teamId = (int)value;
+            return true;
+        }
+        teamId = -1;
+        return false;
     }
 }
