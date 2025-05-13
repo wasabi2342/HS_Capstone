eb39fd10126bf1faee7e5087857d3a2b91dcdbc7
이은석 <a4121240@naver.com>
2025-05-13 16:04:28 +0900

    데이터 저장

--- Assets/SelectDoor.cs
+++ Assets/SelectDoor.cs
@@ -16,6 +16,7 @@ public class SelectDoor : MonoBehaviourPun, IInteractable
         {
             Debug.Log("Reward Door 상호작용 시작 - RewardUI 호출");
             canInteract = false;
+            PhotonNetworkManager.Instance.photonView.RPC("RPC_SaveRunTimeData", RpcTarget.All);
             // PhotonNetworkManager의 PhotonView를 사용하여 모든 클라이언트에 보상 UI를 열도록 RPC 호출
             //PhotonNetworkManager.Instance.photonView.RPC("RPC_OpenRewardUIForAll", RpcTarget.All);
             //InputManager.Instance.ChangeDefaultMap(InputDefaultMap.UI);
