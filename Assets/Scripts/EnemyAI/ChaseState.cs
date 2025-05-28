ee0a095a3335321e84a00bf76cddd53fd2f9d966
wasabi2342 <sj5051715@gmail.com>
2025-05-29 03:17:45 +0900

    사운드 추가 몬스터 버그 수정

--- Assets/Scripts/EnemyAI/ChaseState.cs
+++ Assets/Scripts/EnemyAI/ChaseState.cs
@@ -59,7 +59,6 @@ public class ChaseState : BaseState
             agent.speed = status.moveSpeed * status.chaseSpeedMultiplier;
             agent.stoppingDistance = stopDist;
             agent.autoBraking = false;
-            agent.obstacleAvoidanceType = ObstacleAvoidanceType.LowQualityObstacleAvoidance;
             agent.acceleration = 40;
             agent.angularSpeed = 720;
             agent.SetDestination(transform.position); // 목적지 초기화
